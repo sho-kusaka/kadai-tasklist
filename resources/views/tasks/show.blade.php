78371580cde7288f509f7777ceb1a6a252cb490f
sho.kusaka <hehehey0522@gmail.com>
2019-06-17 14:17:56 +0000

    メッセージ

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -18,11 +18,6 @@
             <td>{{ $task->content }}</td>
         </tr>
     </table>
-    
-    <div class="form-group">
-                    {!! Form::label('status', 'ステータス:') !!}
-                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
-                </div>
                 
      {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id], ['class' => 'btn btn-light']) !!}
 　　{!! Form::model($task,[ 'route' => ['tasks.destroy', $task->id], 'method'=>'delete']) !!}
