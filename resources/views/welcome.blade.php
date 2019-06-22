d663bbcc6a0799501ebce2d3d79350f0a4345701
sho.kusaka <hehehey0522@gmail.com>
2019-06-22 12:00:53 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -76,7 +76,20 @@
                     @endauth
                 </div>
             @endif
+@extends('layouts.app')
 
+@section('content')
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Microposts</h1>
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
+        </div>
+    @endif
+@endsection
             <div class="content">
                 <div class="title m-b-md">
                     Laravel
