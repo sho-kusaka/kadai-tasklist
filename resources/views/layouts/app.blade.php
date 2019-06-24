05e5438d3103399ec37ce4658467a7f78a10def6
sho.kusaka <hehehey0522@gmail.com>
2019-06-24 11:14:17 +0000

    top page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,13 @@
     </head>
 
     <body>
+        @include('commons.navbar')
+        
+        <div class="container">
+            @include('commons.error_tasks')
+            
+            @yield('content')
+            
         <header class="mb-4">
             <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
                 <a class="navbar-brand" href="/">Task</a>
@@ -23,12 +30,7 @@
                 </div>
             </nav>
         </header>
-        
-        <div class="container">
-            @include('commons.error_tasks')
-            @yield('content')
-        </div>
-        
+    
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,22 @@
 
 Route::get('/', 'TasksController@index');
 
-Route::resource('tasks', 'TasksController');
+Route::resource('tasks', 'TasksController');Route::resource('tasks', 'TasksController');
 
 // ログイン認証
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+
+// ユーザ機能
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
